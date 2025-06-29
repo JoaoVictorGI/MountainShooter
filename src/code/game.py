9fa288f2b7ba98266cbbfbab921a16b995baa14a
João Victor <joaovictor.gizidoro@gmail.com>
2025-06-29 15:59:44 -0300

    feat: Created main loop, connecting Menu and Game classes

--- src/code/game.py
+++ src/code/game.py
@@ -1,9 +1,26 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from code.menu import Menu
+
+import pygame
+from pygame.surface import Surface
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window: Surface = pygame.display.set_mode(size=(640, 480))
 
-    def run(self, ):
-        pass
+    def run(
+        self,
+    ):
+        while True:
+            menu: Menu = Menu(self.window)
+            menu.run()
+            pass
+
+        # while True:
+        #     # Check for events
+        #     for event in pygame.event.get():
+        #         if event.type == pygame.QUIT:
+        #             print("Quitting...")
+        #             pygame.quit()  # Close the window
+        #             quit()  # End pygame "process"
--- src/main.py
+++ src/main.py
@@ -1,15 +1,4 @@
-import pygame
-from pygame.surface import Surface
+from code.game import Game
 
-print("Game started")
-pygame.init()
-
-window: Surface = pygame.display.set_mode(size=(640, 480))
-
-print("Start loop")
-while True:
-    # Check for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # Close the window
-            quit()  # End pygame "process"
+game: Game = Game()
+game.run()
